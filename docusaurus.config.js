3828ef706733ca04f64759adfcb1a522a6a53eed
Sacha Clerc-Renaud <sachacr@gmail.com>
2024-08-31 12:42:04 +0200

    Fix domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: "img/ukt/ukt-logo.jpg",
 
   // Set the production url of your site here
-  url: "karate95.com",
+  url: "https://karate95.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
